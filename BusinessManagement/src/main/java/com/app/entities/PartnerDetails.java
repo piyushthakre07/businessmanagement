fb31c6bc8955f547d756c5e205fb0a4d8426c768
Piyush.Thakre <Piyush.Thakre@ISIPL-PUNV-L092.corp.intelliswift.com>
2020-04-16 12:18:14 +0530

    entity

--- BusinessManagement/src/main/java/com/app/entities/PartnerDetails.java
+++ BusinessManagement/src/main/java/com/app/entities/PartnerDetails.java
@@ -19,7 +19,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Setter @Getter
+@Setter
+@Getter
 @Table(name = "partner_details")
 public class PartnerDetails {
 
@@ -27,44 +28,44 @@ public class PartnerDetails {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "partner_details_id", unique = true, nullable = false)
 	private Long partnerDetailsId;
-	
+
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "business_unit_Id")
 	private BusinessUnit businessUnit;
-	
+
 	@Column(name = "partner_name")
 	private String partnerName;
-	
-	@Column(name="partner_contact_number")
+
+	@Column(name = "partner_contact_number")
 	private String partnerContactNumber;
-	
-	@Column(name="partner_address")
+
+	@Column(name = "partner_address")
 	private String partnerAddress;
-	
-	@Column(name="partner_amount_invest")
+
+	@Column(name = "partner_amount_invest")
 	private Integer partnerAmountInvest;
-	
-	@Column(name="partner_percentage")
+
+	@Column(name = "partner_percentage")
 	private Integer partnerPercentage;
-	
-	@Column(name="duration")
+
+	@Column(name = "duration")
 	private String duration;
-	
-	@Column(name="description")
+
+	@Column(name = "description")
 	private String description;
-	
-	@Column(name="active")
-	private String active;	
+
+	@Column(name = "active")
+	private String active;
 	@Column(name = "created_by")
 	private String createdBy;
 
 	@CreationTimestamp
 	@Column(name = "created_date")
 	private Date createdDate;
-	
+
 	@Column(name = "modify_by")
 	private String modifyBy;
-	
+
 	@UpdateTimestamp
 	@Column(name = "modify_date")
 	private Date modifyDate;
